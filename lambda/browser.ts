6a0e2da32e27a90dac75befb8143f873476ebef2
Yoshi <rv@YoshinoMacBook-Air.local>
2023-06-05 17:11:05 +0900

    add layer

--- lambda/browser.ts
+++ lambda/browser.ts
@@ -6,14 +6,16 @@ export async function getBrowser(): Promise<Browser> {
     console.log("Launching browser");
     console.log("IS_LOCAL: " + process.env.IS_LOCAL);
     console.log("BROWSER_PATH: " + process.env.BROWSER_PATH);
+    const executablePath = process.env.IS_LOCAL ? process.env.BROWSER_PATH: await chromium.executablePath();
+    //const executablePath = process.env.IS_LOCAL ? process.env.BROWSER_PATH: "/tmp/chromium";
+
+    console.log("ExecutablePath: " + executablePath);
 
     const browser = await puppeteer.launch({
         args: process.env.IS_LOCAL ? puppeteer.defaultArgs() : chromium.args,
         defaultViewport: chromium.defaultViewport,
-        executablePath: process.env.IS_LOCAL
-            ? process.env.BROWSER_PATH
-            : await chromium.executablePath(),
-        headless: process.env.IS_LOCAL ? false : chromium.headless,
+        executablePath: executablePath,
+        ignoreHTTPSErrors: true
     });
     return browser;
 }
--- lambda/handler/test-handler.ts
+++ lambda/handler/test-handler.ts
@@ -2,16 +2,10 @@ import { getBrowser } from "../browser";
 
 async function fetchData(): Promise<void> {
     const browser = await getBrowser();
-    const page = await browser.newPage();
-    await page.goto("https://www.google.com");  // Await the page load
-    await page.screenshot({                    // Await the screenshot
-        path: "/tmp/screenshot.jpg",
-        fullPage: true,
-    });
-    await browser.close();
+    console.log("Chromium:", await browser.version());
 }
 
 export async function main() {
-    fetchData();
+    await fetchData();
     return {}
 }
