d5861b6616a1264b3331a70d899b5334ebe25de0
Yoshi <rv@YoshinoMacBook-Air.local>
2023-06-08 16:06:25 +0900

    change to one stack

--- bin/jp-serverless-puppeteer.ts
+++ bin/jp-serverless-puppeteer.ts
@@ -1,23 +1,15 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
-import { ChromiumLayerStack } from '../lib/stacks/chromium-layer-stack';
-import { S3Stack, S3StackProps } from '../lib/stacks/s3-stack';
-import { FontLayerStack } from '../lib/stacks/font-layer-stack';
-import { LambdaProps, LambdaStack, LambdaStackProps } from '../lib/stacks/lambda-stack';
+import { ChromiumLayerStack } from '../lib/modules/chromium-layer-stack';
+import { ServerlessPuppeteerStack, ServerlessPuppeteerStackProps, LambdaProps } from '../lib/serverless-puppeteer-stack'; 
 import * as fs from 'fs';
 import * as path from 'path';
 
 export function createServerlessPuppeterStack(app: App) {
-    const chromiumLayer = new ChromiumLayerStack(app, 'ServerlessPuppeterChromiumLayerStack', {});
-    const fontLayer = new FontLayerStack(app, 'ServerlessPuppeterFontLayerStack', {});
+
     const s3BuketName = 'jp-serverless-puppeteer';
     const resourcesPrefix =  'jp-serverless-puppeteer';
-    
-    const s3Stack = new S3Stack(app, 'ServerlessPuppeterS3Stack', {
-        resourcesPrefix: resourcesPrefix,
-        bucketName: s3BuketName,
-    });
 
     const lambdaProps: LambdaProps[] = [
         {
@@ -25,7 +17,6 @@ export function createServerlessPuppeterStack(app: App) {
             handlerPath: path.resolve(__dirname, '..', `./lambda/handler/test-handler.ts`),
             memorySize: 1024,
             useWarmUp: 0,
-            layers: [chromiumLayer.layer, fontLayer.layer],
             environment: {
                 NODE_ENV: 'production',
                 bucket: s3BuketName
@@ -37,14 +28,14 @@ export function createServerlessPuppeterStack(app: App) {
                 externalModules: ['@sparticuz/chromium'],
                 nodeModules: ['puppeteer-core']
             },
-            policySatements: [s3Stack.policySatement]
         },
     ];
 
-    const lambdaStackProps: LambdaStackProps = {
+    const serverlessPuppeteerStackProps: ServerlessPuppeteerStackProps = {
         resourcesPrefix: resourcesPrefix,
+        bucketName: s3BuketName,
         functions: lambdaProps
     };
 
-    new LambdaStack(app, 'ServerlessPuppeterLambdaStack', lambdaStackProps);
+    new ServerlessPuppeteerStack(app, 'ServerlessPuppeterLambdaStack', serverlessPuppeteerStackProps);
 }
--- lib/serverless-puppeteer-stack.ts
+++ lib/serverless-puppeteer-stack.ts
@@ -1,16 +1,89 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { LayerVersion, Runtime, Code, } from 'aws-cdk-lib/aws-lambda';
+import {
+  aws_iam as iam,
+  aws_lambda as lambda,
+  aws_lambda_nodejs as lambdaNodejs,
+  aws_s3 as s3,
+  Duration
+} from 'aws-cdk-lib'
+
+export interface LambdaProps {
+  name: string,
+  handlerPath: string
+  memorySize: number
+  useWarmUp: number
+  bundling?: lambdaNodejs.BundlingOptions
+  environment?: {}
+}
+
+export interface ServerlessPuppeteerStackProps extends cdk.StackProps {
+  resourcesPrefix: string,
+  bucketName: string
+  functions: LambdaProps[]
+}
 
 export class ServerlessPuppeteerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ServerlessPuppeteerStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const s3bucket = new s3.Bucket(this, 'S3Stack', {
+      bucketName: props?.bucketName,
+      versioned: false,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessPuppeteerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fontLayer = new LayerVersion(this, 'FontLayerStack', {
+      code: Code.fromAsset('./fonts/fonts.zip'), // path to your local chromium.zip file
+      compatibleRuntimes: [Runtime.NODEJS_18_X]
+    });
+
+    const versionNumber = "107";
+    const chronumLayer = new LayerVersion(this, 'ChromiumLayer', {
+      code: Code.fromAsset('./chromium/chromium.zip'), // path to your local chromium.zip file
+      compatibleRuntimes: [Runtime.NODEJS_18_X], // Update to your Lambda runtime
+      layerVersionName: `chromium${versionNumber}`,
+      description: `Chromium v${versionNumber}`,
+    });
+
+    // S3 Access Policy
+    const s3PolicySatement = new iam.PolicyStatement({
+      actions: [ // Allow the Lambda function to perform the following S3 actions
+        's3:GetObject',
+        's3:PutObject',
+      ],
+      resources: [ // Restrict access to a specific S3 bucket
+        `arn:aws:s3:::${props?.bucketName}/*`,
+      ],
+      effect: iam.Effect.ALLOW, // Set the effect to "Allow"
+    });
+
+    props.functions.map((lambdaProps: LambdaProps) => {
+      const lambdaExecutionRole = new iam.Role(this, `${props.resourcesPrefix}${lambdaProps.name}LambdaFnExecRole`, {
+        roleName: `${props.resourcesPrefix}_${lambdaProps.name}-exec-role`,
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+        ],
+      });
+      lambdaExecutionRole.addToPolicy(s3PolicySatement);
+
+      new lambdaNodejs.NodejsFunction(this, `${props.resourcesPrefix}${lambdaProps.name}`, {
+        functionName: `${props.resourcesPrefix}-${lambdaProps.name}`,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        timeout: Duration.seconds(60),
+        handler: 'main',
+        role: lambdaExecutionRole,
+        entry: lambdaProps.handlerPath,
+        memorySize: lambdaProps.memorySize,
+        layers: [fontLayer, chronumLayer],
+        bundling: lambdaProps.bundling,
+        environment: {
+          ...lambdaProps.environment
+        },
+      });
+    });
   }
 }
